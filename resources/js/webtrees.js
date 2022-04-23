1209d9e3133d4a583f3de0c2521915b4ffa8b7d9
David Drury <david@drury.me.uk>
2022-04-23 20:16:52 +0100

    Remove function zoomToShowLayer

--- resources/js/webtrees.js
+++ resources/js/webtrees.js
@@ -1140,3 +1140,23 @@ document.addEventListener('click', (event) => {
     }
   }
 });
+
+webtrees.waitForElement = (selector) => {
+  return new Promise(resolve => {
+      if (document.querySelector(selector)) {
+          return resolve(document.querySelector(selector));
+      }
+
+      const observer = new MutationObserver(mutations => {
+          if (document.querySelector(selector)) {
+              resolve(document.querySelector(selector));
+              observer.disconnect();
+          }
+      });
+
+      observer.observe(document.body, {
+          childList: true,
+          subtree: true
+      });
+  });
+}
