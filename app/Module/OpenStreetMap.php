f93739b8b5fcda27335509bb87e19b84d095376f
David Drury <david@drury.me.uk>
2026-04-30 10:51:53 +0100

    Add referer policy

--- app/Module/OpenStreetMap.php
+++ app/Module/OpenStreetMap.php
@@ -67,7 +67,8 @@ class OpenStreetMap extends AbstractModule implements ModuleMapProviderInterface
                 'minZoom'     => 2,
                 'subdomains'  => ['a', 'b', 'c'],
                 'url'         => 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                'localName'   => 'OpenStreetMapsMapnik',
+                'localName'      => 'OpenStreetMapsMapnik',
+                'referrerPolicy' => 'strict-origin-when-cross-origin',
             ],
             (object) [
                 'attribution' => 'Map data ©<a href="https://www.openstreetmap.org">Karte hergestellt aus OpenStreetMap-Daten</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0">CC-BY-SA</a>',
@@ -78,6 +79,7 @@ class OpenStreetMap extends AbstractModule implements ModuleMapProviderInterface
                 'subdomains'  => ['a', 'b', 'c'],
                 'url'         => 'https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png',
                 'localName'   => 'OpenStreetMapsDeutsch',
+                'referrerPolicy' => 'strict-origin-when-cross-origin',
             ],
             (object) [
                 'attribution' => 'Map data ©<a href="https://www.openstreetmap.org">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0">CC-BY-SA</a>',
@@ -88,6 +90,7 @@ class OpenStreetMap extends AbstractModule implements ModuleMapProviderInterface
                 'subdomains'  => ['a', 'b', 'c'],
                 'url'         => 'https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png',
                 'localName'   => 'OpenStreetMapsFrench',
+                'referrerPolicy' => 'strict-origin-when-cross-origin',
             ],
         ];
     }
